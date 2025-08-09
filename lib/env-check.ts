9bdc11f2b9f9bbe5b387b53e6e253de291029d7b
CaboAi <caboconnectai@gmail.com>
2025-08-09 00:37:48 -0700

    feat: add environment check and trigger redeploy after env vars added

---
+++ lib/env-check.ts
@@ -0,0 +1,6 @@
+﻿// Environment check - Added at 00:37:47
+export const ENV_CHECK = {
+  supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
+  hasSupabaseKey: !!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  timestamp: '00:37:47'
+}
