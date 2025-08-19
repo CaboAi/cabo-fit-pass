f949ab7309141bc5e193e885a63e715a3a5c92ad
CaboAi <caboconnectai@gmail.com>
2025-08-19 15:03:01 -0700

    fix: escape apostrophe in signin page to resolve build error

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -176,7 +176,7 @@ export default function SignInPage() {
 
             <div className="text-center">
               <p className="text-sm text-muted-foreground">
-                Don't have an account?{' '}
+              "Don&apos;t have an account? Sign up"{' '}
                 <a href="#" className="text-primary underline-offset-4 hover:underline font-medium">
                   Sign up
                 </a>
